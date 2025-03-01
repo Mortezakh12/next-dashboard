6c95dbd53a3823e334909cf31c543c0cc9c044bc
Mortezakh12 <114151002+Mortezakh12@users.noreply.github.com>
2025-03-01 15:29:48 +0330

    add next canary logic to app

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from 'next';
 import path from 'path';
 
 const nextConfig: NextConfig = {
+  experimental: {
+    ppr: 'incremental'
+  },
   appDir: true,
   webpack: (config: any) => {
     config.resolve.alias['@'] = path.resolve(__dirname);
