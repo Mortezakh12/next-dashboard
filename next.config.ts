a1df0a26d8513f978462127cdfa177c30ce9069b
Mortezakh12 <114151002+Mortezakh12@users.noreply.github.com>
2025-03-05 19:14:39 +0330

    add authentication and login functionality with error handling

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 const nextConfig: NextConfig = {
   experimental: {
-    ppr: 'incremental'
+    // ppr: 'incremental'
   },
   appDir: true,
   webpack: (config: any) => {
